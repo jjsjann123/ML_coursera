22f39a8b458ee465661b7e3dc5b04e56b65c47da
jjsjann123 <alex.jann2012@gmail.com>
2017-04-01 19:29:17 -0700

    [project1_ML] ML_Project_1 requirements finished

--- AndrewNg_ML/machine-learning-ex1/python/project1.py
+++ AndrewNg_ML/machine-learning-ex1/python/project1.py
@@ -1,4 +1,5 @@
 import numpy as np
+from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.pyplot as plt
 
 def loaddata(file):
@@ -8,7 +9,6 @@ def linearRegression(x, y, theta, alpha):
     m = y.size
     theta = theta - alpha / m * np.dot(np.transpose(x), np.dot(x, theta) - y)
     return theta
-    
 
 def computeCostFunction(x, y, theta):
     m = y.size
@@ -37,6 +37,19 @@ for i in range(0, 1500):
     if i % 100 == 0:
         plt.plot([0, t], [theta[0], theta[0]+t*theta[1]]) 
 
+x_cord = np.arange(-10,10,0.025)
+y_cord = np.arange(-1, 4, 0.025)
+X, Y = np.meshgrid(x_cord, y_cord)
+J = np.zeros(X.shape)
+for i in range(1, X.shape[0]):
+    for j in range(1, X.shape[1]):
+        t0 = X[i][j]
+        t1 = Y[i][j]
+        t = np.array([t0, t1])
+        J[i][j] = computeCostFunction(x, y, t)
 
+#plt.figure().gca(projection='3d').plot_surface(X, Y, J)
+v = np.array([1, 2.0, 3.0, 5, 10, 50, 100, 200, 300, 400])
+plt.figure().gca(projection='3d').contour(X, Y, J, v)
 
 plt.show();
